194dbc3f8a64258d938ee97768972aec502b4a73
Twilight <2475911580@qq.com>
2025-12-13 21:54:55 +0800

    init

---
+++ src/main/java/com/seuoj/seuojbackend/SeuojBackendApplication.java
@@ -0,0 +1,13 @@
+package com.seuoj.seuojbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeuojBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeuojBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seuoj/seuojbackend/SeuojBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seuoj.seuojbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeuojBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
